c564f7c1c7b04caec9ab78b8be246785da468386
SirGateHgL <150258745+SirGateHgL@users.noreply.github.com>
2024-06-30 21:42:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cron = require('node-cron');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 80;
@@ -22,8 +23,13 @@ const httpsProxyUrls = [
     'https://raw.githubusercontent.com/cybercrafttool/proxylist/results/all/https.proxy.txt'
 ];
 
-let httpProxies = [];
-let httpsProxies = [];
+const HTTP_FILE = 'http_proxies.txt';
+const HTTPS_FILE = 'https_proxies.txt';
+
+let totalRequestsToday = 0;
+let requestsPerSecond = 0;
+let maxRequestsPerSecond = 0;
+let requestTimestamps = [];
 
 async function fetchProxies(proxyUrls) {
     let proxies = [];
@@ -40,34 +46,82 @@ async function fetchProxies(proxyUrls) {
 }
 
 async function updateProxies() {
-    httpProxies = await fetchProxies(httpProxyUrls);
-    httpsProxies = await fetchProxies(httpsProxyUrls);
-    console.log('Proxy lists updated');
+    const httpProxies = await fetchProxies(httpProxyUrls);
+    const httpsProxies = await fetchProxies(httpsProxyUrls);
+
+    fs.writeFileSync(HTTP_FILE, httpProxies.join('\n'), 'utf8');
+    fs.writeFileSync(HTTPS_FILE, httpsProxies.join('\n'), 'utf8');
+
+    console.log('Proxy files updated');
 }
 
-app.get('/info', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-    res.send('This Things Made by @SirgateHgL');
+function trackRequest() {
+    const now = Date.now();
+    requestTimestamps.push(now);
+    totalRequestsToday++;
+
+    // Remove timestamps older than 1 second
+    requestTimestamps = requestTimestamps.filter(timestamp => now - timestamp <= 1000);
+    requestsPerSecond = requestTimestamps.length;
+    if (requestsPerSecond > maxRequestsPerSecond) {
+        maxRequestsPerSecond = requestsPerSecond;
+    }
+}
+
+app.use((req, res, next) => {
+    trackRequest();
+    next();
 });
 
 app.get('/http', (req, res) => {
     res.set('Content-Type', 'text/plain');
-    res.send(httpProxies.join('\n'));
+    fs.readFile(HTTP_FILE, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading proxy file');
+            return;
+        }
+        res.send(data);
+    });
 });
 
 app.get('/https', (req, res) => {
     res.set('Content-Type', 'text/plain');
-    res.send(httpsProxies.join('\n'));
+    fs.readFile(HTTPS_FILE, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading proxy file');
+            return;
+        }
+        res.send(data);
+    });
 });
 
 app.get('/all', (req, res) => {
     res.set('Content-Type', 'text/plain');
-    const allProxies = [...httpProxies, ...httpsProxies];
-    res.send(allProxies.join('\n'));
+    fs.readFile(HTTP_FILE, 'utf8', (err, httpData) => {
+        if (err) {
+            res.status(500).send('Error reading HTTP proxy file');
+            return;
+        }
+        fs.readFile(HTTPS_FILE, 'utf8', (err, httpsData) => {
+            if (err) {
+                res.status(500).send('Error reading HTTPS proxy file');
+                return;
+            }
+            res.send(httpData + '\n' + httpsData);
+        });
+    });
+});
+
+app.get('/info', (req, res) => {
+    res.json({
+        totalRequestsToday,
+        requestsPerSecond,
+        maxRequestsPerSecond
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     updateProxies(); // Initial fetch
-    cron.schedule('*/10 * * * *', updateProxies); // Fetch every 10 minutes
+    cron.schedule('*/5 * * * *', updateProxies); // Fetch every 5 minutes
 });
